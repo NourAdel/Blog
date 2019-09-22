e641bcf11b7702064e45ccefe8f433964ec7e0f5
NourAdel <noura7305@gmail.com>
2019-09-22 17:48:01 +0200

    post urls

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,7 +1,9 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url, include
+from posts import urls
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+
+urlpatterns = {
+    url(r'admin/', admin.site.urls),
+    url(r'posts/$', posts.urls),
+}
