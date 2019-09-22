8a8b419ab1627ccd48cb73c14832fda7021cffd6
NourAdel <noura7305@gmail.com>
2019-09-22 15:57:24 +0200

    Customize admin

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
+
+class PostAdmin (admin.ModelAdmin):
+    # post attributes displayed to the admin
+    list_display = ["title", "updated", "timestamp"]
+
+    # post attributes ???? to the admin
+    list_display_links = ["updated", "timestamp"]
+    # post attributes the admin can edit
+    list_editable = ["title"]
+    # post attributes the admin can search in
+    list_filter = ["title", "content"]
+
+
+    class Meta:
+        model = Post
 admin.site.register(Post)
