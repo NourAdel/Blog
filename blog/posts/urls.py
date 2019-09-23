c000bdc1490caf7b9c1f7d403216a8f34aa80c4e
NourAdel <noura7305@gmail.com>
2019-09-23 15:42:32 +0200

    making the urls more dynamic

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from . import views
 
+app_name='posts'
+
 urlpatterns = [
 
     url(r'^create/$', views.posts_create),
-    url(r'^detail/$', views.posts_detail),
+    url(r'^(?P<ID>\d+)/$', views.posts_detail, name="detail"),
     url(r'^$', views.posts_list),
     url(r'^update/$', views.posts_update),
     url(r'^delete/$', views.posts_delete),
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
 
 
 # Create your views here.
@@ -9,13 +10,16 @@ def posts_create (request):
     return HttpResponse("hello")
 
 
-def posts_detail(request):
-    return HttpResponse("hello")
+def posts_detail(request, ID):
+    object = get_object_or_404(Post, id=ID)
+    context = {'title': object.title, 'object': object}
+    return render(request, 'post_details.html', context)
 
 
 def posts_list(request):
-    cotext= {'title': 'list'}
-    return render(request, 'index.html', cotext)
+    objlist = Post.objects.all()
+    context = {'title': 'list', 'objects': objlist}
+    return render(request, 'index.html', context)
 
 
 def posts_update (request):
