08b5fe7f68bd11ead434197a2e293cbd8a58d4be
NourAdel <noura7305@gmail.com>
2019-09-23 18:30:34 +0200

    more views

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
     url(r'^create/$', views.posts_create),
     url(r'^(?P<ID>\d+)/$', views.posts_detail, name="detail"),
-    url(r'^$', views.posts_list),
-    url(r'^update/$', views.posts_update),
-    url(r'^delete/$', views.posts_delete),
+    url(r'^$', views.posts_list, name="list"),
+    url(r'^(?P<ID>\d+)/edit/$', views.posts_update, name="update"),
+    url(r'^(?P<ID>\d+)/delete/$', views.posts_delete),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Post
 from .forms import PostForm
-
-
-# Create your views here.
+from django.contrib import messages
 
 
 def posts_create (request):
     form = PostForm(request.POST or None)
     if form.is_valid():
-        instance =  form.save(commit=False)
+        instance = form.save(commit=False)
         instance.save()
+        messages.success(request, "Successfully created")
+        return HttpResponseRedirect(instance.get_absolute_url())
     context = {'form': form}
     return render(request, 'post_form.html', context)
 
@@ -28,9 +28,21 @@ def posts_list(request):
     return render(request, 'index.html', context)
 
 
-def posts_update (request):
-    return HttpResponse("hello")
+def posts_update (request, ID):
+    instance = get_object_or_404(Post, id=ID)
+    form = PostForm(request.POST or None, instance= instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Edit saved")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {'form': form, 'title': instance.title, 'instance': instance}
+    return render(request, 'post_form.html', context)
 
 
-def posts_delete(request):
-    return HttpResponse("hello")
+def posts_delete(request, ID):
+    instance = get_object_or_404(Post, id=ID)
+    instance.delete()
+    messages.success(request, "DELETED")
+    return redirect("posts:list")
+
